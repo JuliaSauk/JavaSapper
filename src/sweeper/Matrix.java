3aabfe5452242a31e104c3db5a5d8c86829fac63
JLobster <sauk.julia@gmail.com>
2018-10-15 00:19:11 +0300

    Update Matrix.java

--- src/sweeper/Matrix.java
+++ src/sweeper/Matrix.java
@@ -5,17 +5,17 @@ class Matrix {
 
      Matrix(Box defaultBox) {
          matrix = new Box[Ranges.getSize().x][Ranges.getSize().y];
-         for (Coordinate coordinate: Ranges.getAllCoordinates()) // заполняем значениями
+         for (Coordinate coordinate: Ranges.getAllCoordinates()) 
              matrix[coordinate.x][coordinate.y] = defaultBox;
      }
 
-    Box get(Coordinate coordinate) { //в указанных координатах вернуть то что там находится
+    Box get(Coordinate coordinate) {
         if (Ranges.inRange(coordinate))
             return matrix[coordinate.x][coordinate.y];
         return null;
     }
 
-    //установит нужное значение в указанную клетку
+   
     void set(Coordinate coordinate, Box box) {
         if (Ranges.inRange(coordinate))
             matrix[coordinate.x][coordinate.y] = box;
